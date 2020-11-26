31a198d17b0161e790aebe4dd92895cb08a1042c
lixingen <lxgbigfish@tom.com>
2020-11-26 13:16:47 +0800

    first commit

---
+++ ECommerce-dev-common/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * All rights Reserved, Designed By alibaba-inc.com
+ *
+ * @author:lixingen
+ * @Title:MySpringStudy
+ * @Package
+ * @Description:练习使用AspectJ注解
+ * @date 2020-02-04
+ */
+public class Test {
+}
